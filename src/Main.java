a68eaa2bd319a157c8c094164a8b34151cab74e5
slash <slasherek55@gmail.com>
2022-07-23 11:47:15 +0200

    dodalem klase main wraz systemoutprintline hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String name = "Dawid";
+        int wiek = 32;
+        System.out.println("Hello "+name+"! Mam " +wiek +"lat.");
+        char a = 'b';
+
     }
 }

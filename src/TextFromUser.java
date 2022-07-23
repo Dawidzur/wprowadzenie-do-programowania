fed6dd80b7f5221cba1bff0fdc7cb35e9fb02c53
slash <slasherek55@gmail.com>
2022-07-23 12:07:03 +0200

    dodalem klase main wraz systemoutprintline hello world

--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class TextFromUser {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        System.out.println("Hello User"+userInput);
+        System.out.println("Podaj imie ");
+        String imie = scanner.nextLine();
+        System.out.println("Podaj nazwisko ");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Podaj wiek ");
+        int wiek = scanner.nextInt();
+        System.out.println("Witaj "+imie +" " +nazwisko+" " +wiek);
     }
 }
